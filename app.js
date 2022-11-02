37904085cb6272eda826a32b9621592f06b85b4c
venkataramireddyyarram <s554991@nwmissouri.edu>
2022-11-02 18:39:57 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var waterRouter = require('./routes/water');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
+
 
 var app = express();
 
@@ -23,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/water', waterRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
